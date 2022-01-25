0278dc39bed20a39230d5479969f09622137836c
ming lu <ming.c.lu@gmail.com>
2022-01-25 15:43:09 -0500

    fasdfads

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -5,20 +5,5 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.", headers={'content-type':'application/json', 'Set-Cookie':'cross-site-cookie=bar; SameSite=None; Secure; HttpOnly', 'Access-Control-Allow-Origin':'https://ambitious-plant-090f10f1e.1.azurestaticapps.net', 'Access-Control-Allow-Credentials':'true'}, status_code=200)
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(f"This HTTP triggered function executed successfully.", headers={'content-type':'application/json', 'Set-Cookie':'cross-site-cookie=bar; SameSite=None; Secure; HttpOnly', 'Access-Control-Allow-Origin':'https://ambitious-plant-090f10f1e.1.azurestaticapps.net', 'Access-Control-Allow-Credentials':'true'}, status_code=200)
+    
